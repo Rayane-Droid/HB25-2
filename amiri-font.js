99ae49611e37431bb56258d456f153109da6bbfc
RAYANE BAKKALI <br.grow.2021@gmail.com>
2025-06-02 19:57:12 +0100

    Update amiri-font.js

--- amiri-font.js
+++ amiri-font.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   window.jspdf.jsPDF.API.events.push(['addFonts', function() {
-    this.addFileToVFS("Amiri-Regular.ttf", "base64");
+    this.addFileToVFS("Amiri-Regular.ttf", "amiriBase64");
     this.addFont("Amiri-Regular.ttf", "Amiri", "normal");
   }]);
 };
