8e746a5d4611845f9fe4641b36c0ee67a11247b0
RAYANE BAKKALI <br.grow.2021@gmail.com>
2025-06-06 14:51:02 +0100

    Update saveToPDF.js

--- saveToPDF.js
+++ saveToPDF.js
@@ -6,26 +6,7 @@
 // ==================================
 
 // ==================================
-function generatePropertyTable() {
-  const t = translations[currentLanguage];
-  const tbody = document.getElementById("property-body");
-  tbody.innerHTML = "";
-  properties.forEach((property, index) => {
-    const row = document.createElement("tr");
-    row.setAttribute("data-checked", "false");
-    const key = property.keys[currentLanguage] || property.keys.fr;
-    const title = property.titles[currentLanguage] || property.titles.fr;
-    const description =
-      property.descriptions[currentLanguage] || property.descriptions.fr;
-    const superficy = generalTranslations.superficy[currentLanguage] || "DH";
-    const area = (property.areas[currentLanguage] || property.areas.fr) + " " + superficy;
-    //const price = property.prices[currentLanguage] || property.prices.fr;
-    const currency = generalTranslations.currencyLabel[currentLanguage] || "DH";
-    const price = (property.prices[currentLanguage] || property.prices.fr) + " " + currency;
-    
-    
-    
-    
+
     
     row.innerHTML = `
           <td>${index + 1}</td>
