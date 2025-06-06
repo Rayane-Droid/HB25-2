394354959097b1d990bfc5235ecf9ef3a4fcb059
RAYANE BAKKALI <br.grow.2021@gmail.com>
2025-06-06 14:27:19 +0100

    Update saveToPDF.js

--- saveToPDF.js
+++ saveToPDF.js
@@ -82,17 +82,7 @@ function submitUserInfo() {
   generatePropertyTable();
 }
 // ==================================
-function resetApp() {
-  currentLanguage = "fr";
-  document.getElementById("language-selection").classList.remove("hidden");
-  document.getElementById("user-form").classList.add("hidden");
-  document.getElementById("content").classList.add("hidden");
-  document.getElementById("language").value = "";
-  document.getElementById("username").value = "";
-  document.getElementById("phone").value = "";
-  document.getElementById("comments").value = "";
-  document.getElementById("property-body").innerHTML = "";
-}
+
 // ==================================
 function generatePropertyTable() {
   const t = translations[currentLanguage];
