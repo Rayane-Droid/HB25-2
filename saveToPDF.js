fc687bf126f473a08343d7364ec1635453c726e9
RAYANE BAKKALI <br.grow.2021@gmail.com>
2025-06-06 14:53:10 +0100

    Update saveToPDF.js

--- saveToPDF.js
+++ saveToPDF.js
@@ -8,34 +8,7 @@
 // ==================================
 
     
-    row.innerHTML = `
-          <td>${index + 1}</td>
-          <td>${key}</td>
-          <td>${area}</td>
-          <td>${price}</td>
-          <td class="validate-cell"></td>
-          <td class="proposal-cell"></td>
-          <td class="cancel-cell"></td>
-        `;
-    // === Appliquer RTL au tableau si la langue est l'arabe ===
-    const table = document.getElementById("property-table");
-    if (currentLanguage === "ar") {
-      table.classList.add("rtl-table");
-      table.setAttribute("dir", "rtl");
-    } else {
-      table.classList.remove("rtl-table");
-      table.removeAttribute("dir");
-    }
-    // ==================================
-    const validateBtn = document.createElement("button");
-    validateBtn.innerText = t.validate;
-    validateBtn.className = "btn";
-    validateBtn.onclick = () => {
-      row.querySelector(".validate-cell").innerText = row.cells[5].innerText;
-      row.querySelector(".proposal-cell").innerHTML = "";
-      row.querySelector(".cancel-cell").innerHTML = "";
-      row.setAttribute("data-checked", "true");
-    };
+    
     const proposeBtn = document.createElement("button");
     proposeBtn.innerText = t.propose;
     proposeBtn.className = "btn";
